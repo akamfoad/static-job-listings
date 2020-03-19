4ae1af106c6a8b5a3e99175a84cba68dc1dc20c8
akamfoad <41629832+akamfoad@users.noreply.github.com>
2020-03-19 23:14:15 +0300

    - App.js updated, from now, this component renders Layout component, instead hardcoded text.
    * components folder added, it holds components to better manager files.
    
    - components/Layout folder added holds files related to
    Layout components.
    
    - Layout.jsx added it renders header and main, also it holds some logic
    for media query events.
    
    - Layout.module.css added, it contains classes for Desktop and Mobile
    version of the Layout component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
+import Layout from "./components/Layout/Layout";
 function App() {
-  return <>App</>;
+  return (
+    <>
+      <Layout>
+        <h1>Main</h1>
+      </Layout>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import classes from "./Layout.module.css";
+const mobileQuery = window.matchMedia("(max-width: 699px)");
+const Layout = props => {
+  const [isMobile, setIsMobile] = useState(mobileQuery.matches);
+  const mobileQueryCallback = e => {
+    setIsMobile(e.matches);
+    console.log(e.matches);
+  };
+  useEffect(() => {
+    mobileQuery.addListener(mobileQueryCallback);
+    return () => {
+      mobileQuery.removeListener(mobileQueryCallback);
+    };
+  }, []);
+  return (
+    <>
+      <header
+        className={[classes.Header, isMobile ? classes.Mobile : null].join(" ")}
+      >
+        {""}
+      </header>
+      <main>{props.children}</main>
+    </>
+  );
+};
+
+export default Layout;
